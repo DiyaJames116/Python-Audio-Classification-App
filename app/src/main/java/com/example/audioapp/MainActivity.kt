5197a8f8fba3793019c23744e0a173fe33f1d5c9
DiyaJames116 <diyaloveschess@gmail.com>
2023-10-21 16:28:52 +0530

    Added button
    Added the button to load and stop displaying the result

--- app/src/main/java/com/example/audioapp/MainActivity.kt
+++ app/src/main/java/com/example/audioapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.audioapp
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.BufferedReader
@@ -11,22 +13,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val dataTextView = findViewById<TextView>(R.id.dataTextView)
+        val loadButton = findViewById<Button>(R.id.loadButton)
+        val stopButton = findViewById<Button>(R.id.stopButton)
 
-        try {
-            val fileInputStream = assets.open("output.txt")
-            val bufferedReader = BufferedReader(InputStreamReader(fileInputStream))
-            val stringBuilder = StringBuilder()
-            var line: String?
+        loadButton.setOnClickListener {
+            try {
+                val fileInputStream = assets.open("output.txt")
+                val bufferedReader = BufferedReader(InputStreamReader(fileInputStream))
+                val stringBuilder = StringBuilder()
+                var line: String?
 
-            while (bufferedReader.readLine().also { line = it } != null) {
-                stringBuilder.append(line).append("\n")
+                while (bufferedReader.readLine().also { line = it } != null) {
+                    stringBuilder.append(line).append("\n")
+                }
+
+                fileInputStream.close()
+                dataTextView.text = stringBuilder.toString()
+
+                // Show the "Stop Display" button and hide the "Load Data" button
+                loadButton.visibility = View.GONE
+                stopButton.visibility = View.VISIBLE
+            } catch (e: Exception) {
+                e.printStackTrace()
+                dataTextView.text = "Failed to read data from the file."
             }
+        }
 
-            fileInputStream.close()
-            dataTextView.text = stringBuilder.toString()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            dataTextView.text = "Failed to read data from the file."
+        stopButton.setOnClickListener {
+            // Clear the data and show the "Load Data" button while hiding the "Stop Display" button
+            dataTextView.text = "Click the 'Load Data' button to load data."
+            loadButton.visibility = View.VISIBLE
+            stopButton.visibility = View.GONE
         }
     }
 }
